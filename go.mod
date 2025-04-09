e978e2d9e96501daaac26a4b65dcf361e8d6f64a
koga1020 <koga.shozo1020@gmail.com>
2025-04-09 23:20:18 +0900

    able to get jira issue detail

--- go.mod
+++ go.mod
@@ -2,9 +2,17 @@ module mcp-sample
 
 go 1.23.3
 
-require github.com/mark3labs/mcp-go v0.18.0
+require (
+	github.com/andygrunwald/go-jira v1.16.0
+	github.com/mark3labs/mcp-go v0.18.0
+)
 
 require (
+	github.com/fatih/structs v1.1.0 // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/trivago/tgo v1.0.7 // indirect
 	github.com/yosida95/uritemplate/v3 v3.0.2 // indirect
 )
---
+++ pkg/jira/issue.go
@@ -0,0 +1,31 @@
+package jira
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/andygrunwald/go-jira"
+	"github.com/mark3labs/mcp-go/mcp"
+	"github.com/mark3labs/mcp-go/server"
+)
+
+func GetIssue(client *jira.Client) (tool mcp.Tool, handler server.ToolHandlerFunc) {
+	return mcp.NewTool("jira_issue",
+			mcp.WithDescription("Get issue details from Jira"),
+			mcp.WithString("issue_key",
+				mcp.Required(),
+				mcp.Description("The issue key to retrieve"),
+			),
+		),
+		func(ctx context.Context, request mcp.CallToolRequest) (*mcp.CallToolResult, error) {
+			issueKey := request.Params.Arguments["issue_key"].(string)
+			issue, _, _ := client.Issue.Get(issueKey, nil)
+			issueJson, err := json.Marshal((issue))
+			if err != nil {
+				return nil, err
+			}
+
+			return mcp.NewToolResultText(string(issueJson)), nil
+
+		}
+}
